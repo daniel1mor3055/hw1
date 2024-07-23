f13802a2f052d58f1b9fde4a21d0a8e673632f14
danielmo <daniel.mor@doubleverify.com>
2024-07-23 23:09:16 +0300

    wikitext pretrain + finetune works

---
+++ s4/directly_on_task/s4_main.py
@@ -0,0 +1,89 @@
+import datetime
+
+import torch
+import wandb
+from torch import nn, optim
+
+from logger import setup_logger
+from s4.directly_on_task.imdb_dataset import get_vocab, get_dataloaders
+from s4.directly_on_task.s4_model import S4Model
+from s4.directly_on_task.s4_train_evaluate import train, evaluate
+
+run_name = f"run_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_s4_directly_on_task"
+
+# Toggle WandB
+use_wandb = False
+
+if use_wandb:
+    # Initialize WandB
+    wandb.login(key="5fda0926085bc8963be5e43c4e501d992e35abe8")
+    wandb.init(project="model-comparison", name=run_name)
+
+# Setup logging
+logger = setup_logger(__name__)
+
+# Hyperparameters
+batch_size = 8
+embed_dim = 64
+hidden_dim = 768
+output_dim = 1
+n_epochs = 2
+num_layers = 1
+learning_rate = 0.001
+
+# Load vocab and data loaders
+vocab = get_vocab()
+train_dataloader, test_dataloader = get_dataloaders(batch_size, vocab)
+
+# Initialize model, criterion, and optimizer
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vocab_size = len(vocab)
+#             d_model=256,
+model = S4Model(
+    d_input=embed_dim,
+    vocab_size=vocab_size,
+    d_output=1,
+    d_model=hidden_dim,
+    n_layers=num_layers,
+    dropout=0.1
+).to(device)
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+logger.info(f"parameter_cnt: {model.count_parameters}")
+
+if use_wandb:
+    # Log hyperparameters and model
+    wandb.config.update(
+        {
+            "run_name": run_name,
+            "batch_size": batch_size,
+            "embed_dim": embed_dim,
+            "hidden_dim": hidden_dim,
+            "output_dim": output_dim,
+            "n_epochs": n_epochs,
+            "learning_rate": learning_rate,
+            "model": "CustomLSTMModel",
+            "parameter_cnt": model.count_parameters,
+        }
+    )
+
+# Training loop
+logger.info("Starting training...")
+for epoch in range(n_epochs):
+    train_loss = train(model, train_dataloader, criterion, optimizer, device, epoch, logger, use_wandb)
+    test_loss = evaluate(model, test_dataloader, criterion, device, logger, use_wandb)
+    logger.info(f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}")
+
+    if use_wandb:
+        # Log metrics to WandB
+        wandb.log({
+            "epoch": epoch + 1,
+            "train_loss": train_loss,
+            "test_loss": test_loss
+        })
+
+logger.info("Training completed.")
+if use_wandb:
+    wandb.finish()
---
+++ s4/lra_pretrain/s4_pretrain.py
@@ -0,0 +1,99 @@
+import datetime
+
+import torch
+import wandb
+from torch import nn, optim
+
+from logger import setup_logger
+from s4.lra_pretrain.imdb_dataset import get_vocab, get_dataloaders
+from s4.lra_pretrain.s4_model import CustomLSTMModel
+from s4.lra_pretrain.s4_pretrain_train_evaluate import train, evaluate
+
+run_name = f"run_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_s4_lra_pretrain"
+
+# Toggle WandB
+use_wandb = False
+
+if use_wandb:
+    # Initialize WandB
+    wandb.login(key="5fda0926085bc8963be5e43c4e501d992e35abe8")
+    wandb.init(project="model-comparison", name=run_name)
+
+# Setup logging
+logger = setup_logger(__name__)
+
+vocab = get_vocab()
+
+# Hyperparameters
+batch_size = 8
+embed_dim = 64
+hidden_dim = 768
+output_dim = len(vocab)
+num_layers = 1
+n_epochs = 2
+learning_rate = 0.001
+
+# Load vocab and data loaders
+train_dataloader, test_dataloader = get_dataloaders(batch_size, vocab)
+
+# Initialize model, criterion, and optimizer
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vocab_size = len(vocab)
+model = CustomLSTMModel(
+    vocab_size=vocab_size,
+    embed_dim=embed_dim,
+    hidden_dim=hidden_dim,
+    num_layers=num_layers
+).to(device)
+
+# TODO - creates dummy lstm_wikitext_pretrained.pth, remove in real training
+# Save the model state dict
+checkpoint_path = "s4_lra_pretrained.pth"
+torch.save(model.state_dict(), checkpoint_path)
+print(f"Dummy checkpoint saved at {checkpoint_path}")
+# TODO - dummy ends here
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+logger.info(f"parameter_cnt: {model.count_parameters}")
+
+if use_wandb:
+    # Log hyperparameters and model
+    wandb.config.update(
+        {
+            "run_name": run_name,
+            "batch_size": batch_size,
+            "embed_dim": embed_dim,
+            "hidden_dim": hidden_dim,
+            "output_dim": vocab_size,
+            "n_epochs": n_epochs,
+            "learning_rate": learning_rate,
+            "model": "CustomLSTMModel",
+            "parameter_cnt": model.count_parameters,
+        }
+    )
+
+# Training loop
+logger.info("Starting training...")
+for epoch in range(n_epochs):
+    train_loss = train(model, train_dataloader, criterion, optimizer, device, epoch, logger, use_wandb)
+    test_loss = evaluate(model, test_dataloader, criterion, device, logger, use_wandb)
+    logger.info(f'Epoch: {epoch + 1}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+
+    if use_wandb:
+        # Log metrics to WandB
+        wandb.log({
+            "epoch": epoch + 1,
+            "train_loss": train_loss,
+            "test_loss": test_loss
+        })
+
+# Save the model checkpoint
+checkpoint_path = "s4_wikitext_pretrained.pth"
+torch.save(model.state_dict(), checkpoint_path)
+logger.info(f"Checkpoint saved at {checkpoint_path}")
+
+logger.info("Training completed.")
+if use_wandb:
+    wandb.finish()
