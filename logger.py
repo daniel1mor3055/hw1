44e9481f1669cf4b24c35bd9fc6b744d464ea963
danielmo <daniel.mor@doubleverify.com>
2024-07-24 21:41:48 +0300

    fixed logger

--- logger.py
+++ logger.py
@@ -1,13 +1,17 @@
 import logging
+import sys
 
+# Setup basic configuration for logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(sys.stdout)  # Ensure logs are directed to stdout
+    ]
 )
 
 
 def setup_logger(name=__name__):
     logger = logging.getLogger(name)
-
     return logger
