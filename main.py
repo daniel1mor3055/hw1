6b9724e19deb1d7673bd489cc46aa045a08d19be
YairVaknin <yairvaknin@mail.tau.ac.il>
2024-07-27 23:17:52 +0300

    dataset classes

--- main.py
+++ main.py
@@ -10,19 +10,37 @@ from models.lstm_model import CustomLSTMModel
 from models.s4_model import S4Model
 from models.transformer_model import CustomTransformerModel
 from utils import replace_final_layer
+from project_datasets.wikitext_dataset import WikiTextDataset
+from project_datasets.imdb_dataset import IMDBDataset
 
 # Command line arguments
 parser = argparse.ArgumentParser(description="Run different models")
 
 # Load config
-with open('config.json') as f:
+with open("config.json") as f:
     config = json.load(f)
 
-parser.add_argument('--model', type=str, required=True, choices=list(config["models"].keys()), help="Model to use")
-parser.add_argument('--run_type', type=str, required=True,
-                    choices=["task", "lra_pretrain", "wikitext_pretrain", "task_finetune_lra_pretrain",
-                             "task_finetune_wikitext_pretrain"], help="Run type")
-parser.add_argument('--use_wandb', action='store_true')
+parser.add_argument(
+    "--model",
+    type=str,
+    required=True,
+    choices=list(config["models"].keys()),
+    help="Model to use",
+)
+parser.add_argument(
+    "--run_type",
+    type=str,
+    required=True,
+    choices=[
+        "task",
+        "lra_pretrain",
+        "wikitext_pretrain",
+        "task_finetune_lra_pretrain",
+        "task_finetune_wikitext_pretrain",
+    ],
+    help="Run type",
+)
+parser.add_argument("--use_wandb", action="store_true")
 args = parser.parse_args()
 
 model_config = config["models"][args.model]
@@ -33,29 +51,39 @@ logger = setup_logger(__name__)
 
 run_name = f"run_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{args.model}_{args.run_type}"
 
-batch_size, n_epochs, learning_rate = run_parameters["batch_size"], run_parameters["n_epochs"], run_parameters[
-    "learning_rate"]
+batch_size, n_epochs, learning_rate = (
+    run_parameters["batch_size"],
+    run_parameters["n_epochs"],
+    run_parameters["learning_rate"],
+)
 
 # Load tokenizer and data loaders
+
 if "wikitext" in args.run_type and "finetune" in args.run_type:
-    from project_datasets.wikitext_dataset import get_tokenizer_and_vocab
-    from project_datasets.imdb_dataset import get_dataloaders
+    get_tokenizer_and_vocab = WikiTextDataset.get_tokenizer_and_vocab
+    get_dataloaders = IMDBDataset.get_dataloaders
 elif "wikitext" in args.run_type:
-    from project_datasets.wikitext_dataset import get_tokenizer_and_vocab, get_dataloaders
+    get_tokenizer_and_vocab = WikiTextDataset.get_tokenizer_and_vocab
+    get_dataloaders = WikiTextDataset.get_dataloaders
 else:
-    from project_datasets.imdb_dataset import get_tokenizer_and_vocab, get_dataloaders
+    get_tokenizer_and_vocab = IMDBDataset.get_tokenizer_and_vocab
+    get_dataloaders = IMDBDataset.get_dataloaders
 tokenizer = get_tokenizer_and_vocab()
 train_dataloader, test_dataloader = get_dataloaders(batch_size, tokenizer)
 
 # Initialize model, criterion, and optimizer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 vocab_size = len(tokenizer.get_vocab())
-finetune = args.run_type in ["task", "task_finetune_lra_pretrain", "task_finetune_wikitext_pretrain"]
+finetune = args.run_type in [
+    "task",
+    "task_finetune_lra_pretrain",
+    "task_finetune_wikitext_pretrain",
+]
 
 model_dic = {
     "lstm": CustomLSTMModel,
     "s4": S4Model,
-    "transformer": CustomTransformerModel
+    "transformer": CustomTransformerModel,
 }
 
 if args.run_type == "task":
@@ -63,7 +91,9 @@ if args.run_type == "task":
 else:
     config["models"][args.model]["output_dim"] = vocab_size
 
-model = model_dic[args.model](vocab_size=vocab_size, **config["models"][args.model], finetune=finetune)
+model = model_dic[args.model](
+    vocab_size=vocab_size, **config["models"][args.model], finetune=finetune
+)
 
 pretrained = "lra_pretrained" if "lra" in args.run_type else "wikitext_pretrained"
 checkpoint_path = f"{args.model}_{pretrained}.pth"
@@ -92,7 +122,8 @@ if args.use_wandb:
                 "parameter_cnt": model.count_parameters,
             },
             **config["models"][args.model],
-            **config["run_parameters"]}
+            **config["run_parameters"],
+        }
     )
 
 # Training loop
@@ -104,17 +135,28 @@ else:
     from train_evaluate.pretrain_train_evaluate import train, evaluate
 
 for epoch in range(n_epochs):
-    train_loss = train(model, train_dataloader, criterion, optimizer, device, epoch, logger, args.use_wandb)
-    test_loss = evaluate(model, test_dataloader, criterion, device, logger, args.use_wandb)
-    logger.info(f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}")
+    train_loss = train(
+        model,
+        train_dataloader,
+        criterion,
+        optimizer,
+        device,
+        epoch,
+        logger,
+        args.use_wandb,
+    )
+    test_loss = evaluate(
+        model, test_dataloader, criterion, device, logger, args.use_wandb
+    )
+    logger.info(
+        f"Epoch: {epoch + 1}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}"
+    )
 
     if args.use_wandb:
         # Log metrics to WandB
-        wandb.log({
-            "epoch": epoch + 1,
-            "train_loss": train_loss,
-            "test_loss": test_loss
-        })
+        wandb.log(
+            {"epoch": epoch + 1, "train_loss": train_loss, "test_loss": test_loss}
+        )
 
 pretrained = "lra_pretrained" if "lra" in args.run_type else "wikitext_pretrained"
 checkpoint_path = f"{args.model}_{pretrained}.pth"
--- project_datasets/wikitext_dataset.py
+++ project_datasets/wikitext_dataset.py
@@ -9,18 +9,22 @@ from torch.utils.data import DataLoader, Dataset
 from logger import setup_logger
 
 # Initialize BPE tokenizer
-tokenizer = Tokenizer(models.BPE())
-tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
-
-trainer = trainers.BpeTrainer(vocab_size=10000, special_tokens=["<unk>", "<pad>", "<s>", "</s>"])
 
 logger = setup_logger(__name__)
 
 
 class WikiTextDataset(Dataset):
+    tokenizer = Tokenizer(models.BPE())
+    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
+
+    trainer = trainers.BpeTrainer(
+        vocab_size=10000, special_tokens=["<unk>", "<pad>", "<s>", "</s>"]
+    )
+
     def __init__(self, split, tokenizer):
-        self.dataset = load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1", split=split).filter(
-            lambda x: x['text'].strip() != '')
+        self.dataset = load_dataset(
+            "Salesforce/wikitext", "wikitext-103-raw-v1", split=split
+        ).filter(lambda x: x["text"].strip() != "")
         self.tokenizer = tokenizer
 
     def __len__(self):
@@ -29,61 +33,66 @@ class WikiTextDataset(Dataset):
     def __getitem__(self, idx):
         return self.dataset[idx]["text"]
 
+    @staticmethod
+    def yield_texts(data_iter):
+        for text in data_iter:
+            yield text
 
-def yield_texts(data_iter):
-    for text in data_iter:
-        yield text
+    @staticmethod
+    def get_tokenizer_and_vocab():
+        tokenizer_file = "wikitext_tokenizer.json"
 
+        # Check if the tokenizer file already exists
+        if os.path.exists(tokenizer_file):
+            logger.info("Tokenizer loaded from file.")
+            return Tokenizer.from_file(tokenizer_file)
 
-def get_tokenizer_and_vocab():
-    tokenizer_file = "wikitext_tokenizer.json"
+        train_iter = load_dataset(
+            "Salesforce/wikitext", "wikitext-103-raw-v1", split="train"
+        ).filter(lambda x: x["text"].strip() != "")["text"]
+        WikiTextDataset.tokenizer.train_from_iterator(
+            WikiTextDataset.yield_texts(train_iter), WikiTextDataset.trainer
+        )
 
-    # Check if the tokenizer file already exists
-    if os.path.exists(tokenizer_file):
-        logger.info("Tokenizer loaded from file.")
-        return Tokenizer.from_file(tokenizer_file)
+        # Save the tokenizer to a file
+        WikiTextDataset.tokenizer.save(tokenizer_file)
+        logger.info("Tokenizer trained and saved to file.")
 
-    train_iter = \
-    load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1", split="train").filter(lambda x: x['text'].strip() != '')[
-        "text"]
-    tokenizer.train_from_iterator(yield_texts(train_iter), trainer)
+        return WikiTextDataset.tokenizer
 
-    # Save the tokenizer to a file
-    tokenizer.save(tokenizer_file)
-    logger.info("Tokenizer trained and saved to file.")
+    @staticmethod
+    def text_pipeline(text, tokenizer):
+        return tokenizer.encode(text).ids
 
-    return tokenizer
+    @staticmethod
+    def collate_batch(batch, tokenizer):
+        text_list = []
+        for _text in batch:
+            processed_text = torch.tensor(
+                WikiTextDataset.text_pipeline(_text, tokenizer), dtype=torch.int64
+            )
+            text_list.append(processed_text)
+        # Dummy labels for wikitext
+        return torch.zeros(len(text_list), dtype=torch.int64), pad_sequence(
+            text_list, padding_value=tokenizer.token_to_id("<pad>"), batch_first=True
+        )
 
+    @staticmethod
+    def get_dataloaders(batch_size):
+        tokenizer = WikiTextDataset.tokenizer
+        train_dataset = WikiTextDataset(split="train", tokenizer=tokenizer)
+        test_dataset = WikiTextDataset(split="test", tokenizer=tokenizer)
 
-def text_pipeline(text, tokenizer):
-    return tokenizer.encode(text).ids
-
-
-def collate_batch(batch, tokenizer):
-    text_list = []
-    for _text in batch:
-        processed_text = torch.tensor(text_pipeline(_text, tokenizer), dtype=torch.int64)
-        text_list.append(processed_text)
-    # Dummy labels for wikitext
-    return torch.zeros(len(text_list), dtype=torch.int64), pad_sequence(text_list,
-                                                                        padding_value=tokenizer.token_to_id("<pad>"),
-                                                                        batch_first=True)
-
-
-def get_dataloaders(batch_size, tokenizer):
-    train_dataset = WikiTextDataset(split="train", tokenizer=tokenizer)
-    test_dataset = WikiTextDataset(split="test", tokenizer=tokenizer)
-
-    train_dataloader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        collate_fn=lambda x: collate_batch(x, tokenizer),
-    )
-    test_dataloader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        collate_fn=lambda x: collate_batch(x, tokenizer),
-    )
-    return train_dataloader, test_dataloader
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=lambda x: WikiTextDataset.collate_batch(x, tokenizer),
+        )
+        test_dataloader = DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=lambda x: WikiTextDataset.collate_batch(x, tokenizer),
+        )
+        return train_dataloader, test_dataloader
